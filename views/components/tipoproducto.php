1869a25694ba6f9d2e943356fabc866fde0ccd3b
Eduartch <eduarenriquet@gmail.com>
2026-09-08 17:39:59 -0500

    2026-09-08

--- views/components/tipoproducto.php
+++ views/components/tipoproducto.php
@@ -1,7 +1,8 @@
 <label for="">Tipo de Prod:</label>
 <div>
     <select class="form-control form-control-sm" id="cmbtipoproducto" name="cmbtipoproducto" aria-label=".form-select-lg example">
-        <option <?php echo ($ctipp == 'K' ? 'selected' : '') ?> value="K">KARDEX</option>
+        <option <?php echo ($ctipp == 'K' ? 'selected' : '') ?> value="K">TODO</option>
+        <option <?php echo ($ctipp == 'C' ? 'selected' : '') ?> value="C">COMBUSTIBLE</option>
         <!-- <?php if ($_SESSION['config']['combos'] == 'S') : ?>
             <option <?php echo ($ctipp == 'C' ? 'selected' : '') ?> value="C">COMBO</option>
         <?php endif; ?> -->
