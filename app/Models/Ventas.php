6ee1801baa6e5f4901cfda9760d5144ad3aaf608
Eduartch <eduarenriquet@gmail.com>
2026-09-21 14:33:42 -0500

    2026-09-21

--- app/Models/Ventas.php
+++ app/Models/Ventas.php
@@ -2007,9 +2007,7 @@ class Ventas extends Modelo
 
             // Ejecutar procedimiento almacenado
             $sql = "CALL ProConvierteDocumentoaFactura(:id,:tipodocumento,:documento,:documentoantiguo,:formapago,:usuario)";
-
             $exec = $pdo->prepare($sql);
-
             $exec->execute([
                 'id' => $idauto,
                 'tipodocumento' => $cmbtdoc,
